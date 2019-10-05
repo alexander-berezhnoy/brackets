b0a52b93f94cc7bf491500f773e1010149b4a1ef
alexander-berezhnoy <berrezhnoy@gmail.com>
2019-10-05 17:04:46 +0300

    feat: almost solved task 18/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    let open = {};
+    let closed = {};
+    let stack = [];
+
+    bracketsConfig.forEach(brackets => {
+        open[brackets[0]] = brackets[1];
+        closed[brackets[1]] = true;
+    });
+
+    let wasOpened = false;
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (char === open[char]) {
+            if (!wasOpened) {
+                stack.push(char);
+                wasOpened = true;
+            } else {
+                wasOpened = false;
+                if (open[stack.pop()] !== char) return false;
+            }
+        } else {
+            if (open[char]) {
+                stack.push(char);
+            } else if (closed[char]) {
+                if (open[stack.pop()] !== char) return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
